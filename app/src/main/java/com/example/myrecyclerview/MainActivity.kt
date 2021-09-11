8a1769136a0b2b37b75217cfcdd8ac2a7444cab7
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-11 18:23:27 +0700

    Memanggil data yang sudah kita buat di kelas HeroesData.kt

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +17,15 @@ class MainActivity : AppCompatActivity() {
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
 
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+
     }
+
+    private fun showRecyclerList () {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+    }
+
 }
